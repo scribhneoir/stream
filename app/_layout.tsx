74a8590bebcf7103ba975f0dd89f3ee447bfb2ba
Josiah McCracken <scribhneoir21@gmail.com>
2024-03-13 22:21:58 -0400

    fix: drawer animations

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,9 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import Animated, {
+  LinearTransition,
   ReduceMotion,
+  useAnimatedStyle,
   useSharedValue,
   withSpring,
 } from 'react-native-reanimated';
@@ -21,9 +23,10 @@ export default function App() {
 
   const [drawerOpen, setDrawerOpen] = useState(false);
   const width = useSharedValue(0);
+
   const toggleDrawer = () => {
     width.value = drawerOpen
-      ? withSpring(300, {
+      ? withSpring(0, {
           duration: 300,
           dampingRatio: 1,
           stiffness: 100,
@@ -32,7 +35,7 @@ export default function App() {
           restSpeedThreshold: 2,
           reduceMotion: ReduceMotion.Never,
         })
-      : withSpring(0, {
+      : withSpring(300, {
           duration: 300,
           dampingRatio: 1,
           stiffness: 100,
@@ -43,47 +46,60 @@ export default function App() {
         });
     setDrawerOpen(!drawerOpen);
   };
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      width: width.value,
+    };
+  });
+
   return (
     <View style={{ backgroundColor: 'black', height: '100%', width: '100%' }}>
       <SafeAreaView
         style={{ position: 'relative', height: '100%', width: '100%' }}
       >
         <View style={{ position: 'relative', height: '100%', width: '100%' }}>
-          <Entypo
-            onPress={toggleDrawer}
-            name='menu'
-            size={24}
-            color='white'
-          />
-          {/* <View
-            className={`absolute left-0 flex flex-col ${drawerOpen ? 'w-[300px] max-w-[300px]' : 'max-w-0'} z-10 h-full overflow-hidden bg-gray-800 transition-transform`}
-          > */}
           <Animated.View
-            style={{
-              position: 'absolute',
-              left: 0,
-              display: 'flex',
-              flexDirection: 'column',
-              zIndex: 10,
-              height: '100%',
-              width,
-              overflow: 'hidden',
-              backgroundColor: '#374151',
-            }}
+            layout={LinearTransition}
+            style={[
+              animatedStyles,
+              {
+                position: 'absolute',
+                left: 0,
+                display: 'flex',
+                flexDirection: 'column',
+                zIndex: 10,
+                height: '100%',
+                overflow: 'hidden',
+                backgroundColor: '#374151',
+                borderTopRightRadius: 10,
+                borderBottomRightRadius: 10,
+              },
+            ]}
           >
+            <View style={{ padding: 10, width: 300 }}>
+              <Entypo
+                onPress={toggleDrawer}
+                name='menu'
+                size={24}
+                color='white'
+              />
+              <Text style={{ color: 'white' }}>Drawer</Text>
+            </View>
+          </Animated.View>
+          <View style={{ padding: 10 }}>
             <Entypo
               onPress={toggleDrawer}
               name='menu'
               size={24}
               color='white'
             />
-            <Text style={{ color: 'white' }}>Drawer</Text>
-          </Animated.View>
-          <Text style={{ color: 'white' }}>Hello, World!</Text>
-          <StatusBar style='light' />
-          <Slot />
+            <Text style={{ color: 'white' }}>Hello, World!</Text>
+            <Slot />
+          </View>
         </View>
       </SafeAreaView>
+      <StatusBar style='auto' />
     </View>
   );
 }
