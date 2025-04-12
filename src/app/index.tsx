ab2eb4af4f15329e358068b7a77d6dcd80b30189
Josiah McCracken <scribhneoir21@gmail.com>
2025-04-12 07:53:02 -0400

    feat: write title after file is done

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 import { LinearGradient } from 'expo-linear-gradient';
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import {
 	type NativeSyntheticEvent,
 	Platform,
@@ -8,6 +8,13 @@ import {
 	type TextInputKeyPressEventData,
 	View,
 } from 'react-native';
+import Animated, {
+	ReduceMotion,
+	SlideInDown,
+	SlideInUp,
+	SlideOutDown,
+	SlideOutUp,
+} from 'react-native-reanimated';
 import { useFileStorage } from '../providers/FileStorage';
 
 export default function Flow() {
@@ -17,6 +24,7 @@ export default function Flow() {
 	const [tags, setTags] = useState<string>('');
 	const [enterCount, setEnterCount] = useState(0);
 	const [tagInit, setTagInit] = useState(false);
+	const [showTitle, setShowTitle] = useState(false);
 
 	const { readFile, writeFile } = useFileStorage();
 
@@ -29,38 +37,44 @@ export default function Flow() {
 		setTitle('');
 		setText('');
 		setTags('');
+		setShowTitle(false);
 		setEnterCount(0);
 		setTagInit(false);
-		ref_title.current?.focus();
 	};
 
 	//todo: async storage for title and current note
+
 	const handleTitleChange = (t: string) => {
 		const newTitle = t.replace(' ', '.').toLocaleLowerCase();
 		setTitle(newTitle);
 	};
 
+	//todo: title suggestions
+
 	const handleSave = async () => {
+		if (!title.trim()) return;
 		const oldTitle = title;
 		const oldTags = tags;
 		const oldText = text.trim();
 		const titleArray = title.split('.');
 		handleStateReset();
+		ref_title.current?.blur();
 
 		try {
 			let data = await readFile(`${title}.md`);
 			if (!data.trim()) {
-				data = `---\ntitle: ${oldTitle}\ntags: ${oldTags.replaceAll(
+				data = `---\ntitle: ${oldTitle}\ndate: ${new Date().toLocaleDateString()}\ndraft: true\ntags: ${oldTags.replaceAll(
 					'#',
-					'\\#',
+					'\n  - ',
 				)}\n---\n# ${titleArray[titleArray.length - 1]}`;
 			}
-			data = `${data}\n### ${new Date().toLocaleDateString()}\n${oldText}`;
+			data = `${data}\n\n### ${new Date().toLocaleDateString()}\n${oldText}`;
 			const ancientTags = data.match(/^(tags:)(.+)/m)?.[0] || '';
 			data.replaceAll(
 				ancientTags,
 				`${ancientTags}${oldTags.replaceAll('#', '\\#')}`,
 			);
+			data.replace(/(date:)(.+)/m, `$1 ${new Date().toLocaleDateString()}`);
 			await writeFile(`${title}.md`, data);
 		} catch (e) {
 			console.log(e);
@@ -72,7 +86,8 @@ export default function Flow() {
 	) => {
 		if (e.nativeEvent.key === 'Enter') {
 			if (enterCount >= 3) {
-				handleSave();
+				setShowTitle(true);
+				// handleSave();
 			} else {
 				setEnterCount(enterCount + 1);
 			}
@@ -99,9 +114,42 @@ export default function Flow() {
 		}
 	};
 
+	const handleTitleKeyPress = (
+		e: NativeSyntheticEvent<TextInputKeyPressEventData>,
+	) => {
+		if (
+			e.nativeEvent.key === 'Escape' ||
+			((e.nativeEvent.key === 'Backspace' || e.nativeEvent.key === 'Delete') &&
+				!title.trim())
+		) {
+			setTitle('');
+			setShowTitle(false);
+			ref_title.current?.blur();
+		}
+	};
+
+	const setWebInputDim = () => {
+		if (ref_text.current && Platform.OS === 'web') {
+			// @ts-ignore - accessing DOM properties
+			const element = ref_text.current;
+			// @ts-ignore - accessing DOM properties
+			element.style.height = '0px';
+			// @ts-ignore - accessing DOM properties
+			element.style.height = `${element.scrollHeight}px`;
+		}
+	};
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		if (!showTitle) {
+			setWebInputDim();
+		}
+	}, [showTitle]);
+
 	return (
 		<View
 			style={{
+				position: 'relative',
 				display: 'flex',
 				height: '100%',
 				width: '100%',
@@ -109,142 +157,166 @@ export default function Flow() {
 				justifyContent: 'flex-start',
 			}}
 		>
-			<TextInput
-				value={title}
-				ref={ref_title}
-				keyboardAppearance='dark'
-				onChangeText={(t) => handleTitleChange(t)}
-				placeholder='title'
-				placeholderTextColor='#353835'
-				enterKeyHint='done'
-				autoFocus
-				autoCorrect={false}
-				autoComplete='off'
-				spellCheck={false}
-				onSubmitEditing={() =>
-					title ? ref_text.current?.focus() : ref_title.current?.focus()
-				}
-				style={[
-					{
-						backgroundColor: 'black',
-						fontFamily: 'spB',
-						width: '100%',
-						color: '#B8C2B9',
-						fontSize: 25,
-					},
-					Platform.OS === 'web'
-						? {
-								//web-only style props
-								// @ts-ignore
-								scrollbarWidth: 'none',
-								// @ts-ignore
-								outlineStyle: 'none',
-							}
-						: {},
-				]}
-			/>
-			<TextInput
-				value={tags}
-				ref={ref_tags}
-				keyboardAppearance='dark'
-				onChangeText={(t) => setTags(t)}
-				enterKeyHint='done'
-				autoCorrect={false}
-				autoComplete='off'
-				spellCheck={false}
-				onSubmitEditing={() =>
-					title ? ref_text.current?.focus() : ref_title.current?.focus()
-				}
-				style={[
-					{
-						backgroundColor: 'black',
-						fontFamily: 'spB',
-						width: '100%',
-						color: '#353835',
-						fontSize: 14,
-					},
-					Platform.OS === 'web'
-						? {
-								//web-only style props
-								// @ts-ignore
-								scrollbarWidth: 'none',
-								// @ts-ignore
-								outlineStyle: 'none',
-							}
-						: {},
-				]}
-			/>
-			<Pressable
-				style={{
-					position: 'relative',
-					display: 'flex',
-					height: '90%',
-					width: '100%',
-					maxWidth: 500,
-					alignItems: 'center',
-					justifyContent: 'flex-start',
-				}}
-				onPress={() => {
-					title && ref_text.current?.focus();
-				}}
-			>
-				<TextInput
-					value={text}
-					keyboardAppearance='dark'
-					multiline
-					showsHorizontalScrollIndicator={false}
-					showsVerticalScrollIndicator={false}
-					autoCorrect={false}
-					autoComplete='off'
-					spellCheck={false}
-					ref={ref_text}
-					onKeyPress={(e) => handleTextKeyPress(e)}
-					onChange={(e) => {
-						if (!reset) {
-							setText(e.nativeEvent.text);
-							if (Platform.OS === 'web') {
-								// @ts-ignore
-								e.nativeEvent.target.style.height = 0;
-								// @ts-ignore
-								e.nativeEvent.target.style.height = `${e.nativeEvent.target.scrollHeight}px`;
-							}
-						} else {
-							setReset(false);
-						}
-					}}
-					style={[
-						{
-							fontFamily: 'sp',
-							width: '100%',
-							position: 'absolute',
-							bottom: '50%',
-							color: '#B8C2B9',
-							fontSize: 18,
-							verticalAlign: 'bottom',
-							paddingTop: 0,
-							paddingBottom: 0,
-						},
-						Platform.OS === 'web'
-							? {
-									//web-only style props
-									// @ts-ignore
-									scrollbarWidth: 'none',
-									// @ts-ignore
-									outlineStyle: 'none',
-								}
-							: {},
-					]}
-				/>
-				<LinearGradient
-					colors={['black', 'transparent']}
-					locations={[0.5, 1]}
+			{showTitle && (
+				<Animated.View
+					entering={SlideInDown.springify().reduceMotion(ReduceMotion.Never)}
+					exiting={SlideOutDown.springify().reduceMotion(ReduceMotion.Never)}
 					style={{
-						width: '100%',
-						height: '50%',
-						pointerEvents: 'none',
+						borderRadius: 10,
+						borderWidth: 2,
+						borderColor: '#353835',
+						overflow: 'hidden',
+						paddingHorizontal: 7,
+						position: 'absolute',
+						top: '30%',
+						zIndex: 1,
 					}}
-				/>
-			</Pressable>
+				>
+					<TextInput
+						value={title}
+						ref={ref_title}
+						keyboardAppearance='dark'
+						onKeyPress={(e) => handleTitleKeyPress(e)}
+						onChangeText={(t) => handleTitleChange(t)}
+						placeholder='title'
+						placeholderTextColor='#353835'
+						enterKeyHint='done'
+						autoFocus
+						autoCorrect={false}
+						autoComplete='off'
+						spellCheck={false}
+						onSubmitEditing={handleSave}
+						style={[
+							{
+								backgroundColor: 'black',
+								fontFamily: 'spB',
+								width: '100%',
+								color: '#B8C2B9',
+								fontSize: 25,
+							},
+							Platform.OS === 'web'
+								? {
+										//web-only style props
+										// @ts-ignore
+										scrollbarWidth: 'none',
+										// @ts-ignore
+										outlineStyle: 'none',
+									}
+								: {},
+						]}
+					/>
+				</Animated.View>
+			)}
+			{!showTitle && (
+				<Animated.View
+					entering={SlideInUp.springify().reduceMotion(ReduceMotion.Never)}
+					exiting={SlideOutUp.springify().reduceMotion(ReduceMotion.Never)}
+					style={{
+						display: 'flex',
+						alignItems: 'center',
+						justifyContent: 'flex-start',
+						height: '90%',
+						width: '100%',
+						maxWidth: 500,
+					}}
+				>
+					<TextInput
+						value={tags}
+						ref={ref_tags}
+						keyboardAppearance='dark'
+						onChangeText={(t) => setTags(t)}
+						enterKeyHint='done'
+						autoCorrect={false}
+						autoComplete='off'
+						spellCheck={false}
+						onSubmitEditing={() => ref_text.current?.focus()}
+						style={[
+							{
+								backgroundColor: 'black',
+								fontFamily: 'spB',
+								width: '100%',
+								color: '#353835',
+								fontSize: 14,
+							},
+							Platform.OS === 'web'
+								? {
+										//web-only style props
+										// @ts-ignore
+										scrollbarWidth: 'none',
+										// @ts-ignore
+										outlineStyle: 'none',
+									}
+								: {},
+						]}
+					/>
+					<Pressable
+						style={{
+							width: '100%',
+							height: '100%',
+							position: 'relative',
+							display: 'flex',
+							alignItems: 'center',
+							justifyContent: 'flex-start',
+							overflow: 'hidden',
+						}}
+						onPress={() => {
+							ref_text.current?.focus();
+						}}
+					>
+						<TextInput
+							value={text}
+							keyboardAppearance='dark'
+							multiline
+							showsHorizontalScrollIndicator={false}
+							showsVerticalScrollIndicator={false}
+							autoCorrect={false}
+							autoComplete='off'
+							autoFocus
+							spellCheck={false}
+							ref={ref_text}
+							onKeyPress={(e) => handleTextKeyPress(e)}
+							onChange={(e) => {
+								if (!reset) {
+									setText(e.nativeEvent.text);
+									setWebInputDim();
+								} else {
+									setReset(false);
+								}
+							}}
+							style={[
+								{
+									fontFamily: 'sp',
+									width: '100%',
+									position: 'absolute',
+									bottom: '50%',
+									color: '#B8C2B9',
+									fontSize: 18,
+									verticalAlign: 'bottom',
+									paddingTop: 0,
+									paddingBottom: 0,
+								},
+								Platform.OS === 'web'
+									? {
+											//web-only style props
+											// @ts-ignore
+											scrollbarWidth: 'none',
+											outlineStyle: 'none',
+										}
+									: {},
+							]}
+						/>
+						<LinearGradient
+							colors={['black', 'transparent']}
+							locations={[0.5, 1]}
+							style={{
+								width: '100%',
+								height: '50%',
+								pointerEvents: 'none',
+							}}
+						/>
+					</Pressable>
+				</Animated.View>
+			)}
 		</View>
 	);
 }
