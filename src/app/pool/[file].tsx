33552ac78a8e3686ce1a0a8a77d8bb96e7ec9c97
Josiah McCracken <scribhneoir21@gmail.com>
2025-04-18 08:19:34 -0400

    feat: scroll and avoid keyboard

--- src/app/pool/[file].tsx
+++ src/app/pool/[file].tsx
@@ -1,6 +1,6 @@
 import { useLocalSearchParams } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { Platform, TextInput, View } from 'react-native';
+import { KeyboardAvoidingView, Platform, TextInput, View } from 'react-native';
 import { useFileStorage } from '../../providers/FileStorage';
 
 export default function Pool() {
@@ -29,49 +29,67 @@ export default function Pool() {
 	//todo: add tags
 
 	return (
-		<View
-			style={{
-				display: 'flex',
-				height: '90%',
-				width: '100%',
-				alignItems: 'center',
-				justifyContent: 'flex-start',
-			}}
+		<KeyboardAvoidingView
+			behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+			style={{ flex: 1 }}
 		>
-			<TextInput
-				value={text}
-				onChangeText={handleChange}
-				keyboardAppearance='dark'
-				multiline
-				showsHorizontalScrollIndicator={false}
-				showsVerticalScrollIndicator={false}
-				autoCorrect={false}
-				autoComplete='off'
-				spellCheck={false}
+			<View
 				style={[
 					{
-						fontFamily: 'sp',
+						display: 'flex',
+						height: '90%',
 						width: '100%',
-						position: 'absolute',
-						height: '100%',
-						maxWidth: 500,
-						color: '#B8C2B9',
-						fontSize: 18,
-						verticalAlign: 'bottom',
-						paddingTop: 0,
-						paddingBottom: 0,
+						alignItems: 'center',
+						justifyContent: 'flex-start',
+						overflow: 'scroll',
+						paddingBottom: 10,
 					},
 					Platform.OS === 'web'
 						? {
 								//web-only style props
 								// @ts-ignore
-								scrollbarWidth: 'none',
-								// @ts-ignore
-								outlineStyle: 'none',
+								scrollbarColor: '#B8C2B9 #111211',
+								overflowY: 'scroll',
+								overflowX: 'hidden',
 							}
 						: {},
 				]}
-			/>
-		</View>
+			>
+				<TextInput
+					value={text}
+					onChangeText={handleChange}
+					keyboardAppearance='dark'
+					multiline
+					showsHorizontalScrollIndicator={false}
+					showsVerticalScrollIndicator={false}
+					autoCorrect={false}
+					autoComplete='off'
+					spellCheck={false}
+					style={[
+						{
+							fontFamily: 'sp',
+							width: '100%',
+							position: 'absolute',
+							height: '100%',
+							maxWidth: 500,
+							color: '#B8C2B9',
+							fontSize: 18,
+							verticalAlign: 'bottom',
+							paddingTop: 0,
+							paddingBottom: 0,
+						},
+						Platform.OS === 'web'
+							? {
+									//web-only style props
+									// @ts-ignore
+									scrollbarWidth: 'none',
+									// @ts-ignore
+									outlineStyle: 'none',
+								}
+							: {},
+					]}
+				/>
+			</View>
+		</KeyboardAvoidingView>
 	);
 }
