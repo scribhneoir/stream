07b697213a75427ab66b140194ba7605a10d331f
Josiah McCracken <scribhneoir21@gmail.com>
2025-05-12 22:05:04 -0400

    feat: parse yaml frontmatter

--- src/app/pool/[file].tsx
+++ src/app/pool/[file].tsx
@@ -1,32 +1,73 @@
-import { useLocalSearchParams } from 'expo-router';
-import { useEffect, useState } from 'react';
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import { useEffect, useRef, useState } from 'react';
 import { KeyboardAvoidingView, Platform, TextInput, View } from 'react-native';
+import { parse, stringify } from 'yaml';
+import { PoolTitle } from '../../components/pool/Title';
 import { useFileStorage } from '../../providers/FileStorage';
 
 export default function Pool() {
 	const { file } = useLocalSearchParams();
 	const { readFile, writeFile } = useFileStorage();
 	const [text, setText] = useState<string>('');
+	const [title, setTitle] = useState<string>('');
+	const [tags, setTags] = useState<string[]>([]);
+	const [draft, setDraft] = useState(true);
+	const router = useRouter();
+
+	const debounceTimeout = useRef<NodeJS.Timeout | null>(null);
 
 	const handleChange = async (newText: string) => {
 		setText(newText);
-		writeFile(`${file}.md`, newText);
+		//debounce
+
+		if (debounceTimeout.current) {
+			clearTimeout(debounceTimeout.current);
+		}
+
+		debounceTimeout.current = setTimeout(async () => {
+			await writeFile(`${file}.md`, fileData());
+		}, 300); // Adjust debounce delay as needed
 	};
 
+	const fileData = () =>
+		`---\n${stringify({ title, draft, tags })}---\n${text}`;
+
+	const handleTitleBlur = async () => {
+		const frontmatter = { title, draft, tags };
+		let textToWrite = text.trim();
+
+		const existingFileData = await readFile(`${title}.md`);
+		if (existingFileData) {
+			const [_, existingFrontmatter, existingText] =
+				existingFileData.split('---');
+			const existingFrontmatterData = parse(existingFrontmatter);
+			frontmatter.tags.push(...(existingFrontmatterData.tags || []));
+			textToWrite = `${existingText.trim()}\n${textToWrite}`;
+		}
+		await writeFile(
+			`${title}.md`,
+			`---\n${stringify(frontmatter)}---\n${textToWrite}`,
+		);
+		router.navigate(`/pool/${title}`);
+	};
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
 	useEffect(() => {
 		(async () => {
 			const data = await readFile(`${file}.md`);
-			setText(data);
+			const [_, frontmatter, text] = data.split('---');
+			if (text) {
+				setText(text.trim());
+			}
+
+			if (frontmatter) {
+				const frontmatterData = parse(frontmatter);
+				setTitle(frontmatterData?.title ?? '');
+				setDraft(frontmatterData?.draft === 'true');
+				setTags(frontmatterData?.tags ?? []);
+			}
 		})();
-	}, [file, readFile]);
-
-	//todo: add title rename
-
-	//todo: render frontmatter seperately
-
-	//todo: render markdown
-
-	//todo: add tags
+	}, [file]);
 
 	return (
 		<KeyboardAvoidingView
@@ -43,6 +84,7 @@ export default function Pool() {
 						justifyContent: 'flex-start',
 						overflow: 'scroll',
 						paddingBottom: 10,
+						gap: 10,
 					},
 					Platform.OS === 'web'
 						? {
@@ -55,6 +97,10 @@ export default function Pool() {
 						: {},
 				]}
 			>
+				<PoolTitle title={title} setTitle={setTitle} onBlur={handleTitleBlur} />
+				{/* todo: options */}
+				{/* todo: tags */}
+				{/* todo: live render markdown */}
 				<TextInput
 					value={text}
 					onChangeText={handleChange}
@@ -69,7 +115,6 @@ export default function Pool() {
 						{
 							fontFamily: 'sp',
 							width: '100%',
-							position: 'absolute',
 							height: '100%',
 							maxWidth: 500,
 							color: '#B8C2B9',
